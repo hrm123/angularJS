1f8ffbe5e735445cda40b3216b3446e9b43b9045
bestc0der <hrammohan@yahoo.com>
2016-10-24 15:41:38 -0400

    delta

--- blogzilla/app.js
+++ blogzilla/app.js
@@ -1,5 +1,14 @@
 ( function(){
     var bApp = angular.module('BloggerApp',['ngMaterial']);
+    
+    bApp.config( function($mdIconProvider){
+        $mdIconProvider.defaultIconSet('./svg/avatars.svg',128);
+        $mdIconProvider.icon('share','./svg/share.svg',24);
+            
+          
+    });
+    
+    
     bApp.run(function(){
         console.log("you can start blogging");
     });     
